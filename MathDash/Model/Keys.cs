4bf379b075f110d0e4b88ff432ea474a4aed5567
Matthew Michihara <matthew.michihara@gmail.com>
2012-04-08 16:14:35 -0700

    Actually keep this checked in, but don't track new changes to it

---
+++ MathDash/Model/Keys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MathDash.Model
+{
+    public class Keys
+    {
+        // Replace with actual salt. Do not check in with actual salt.
+        public static string SERVER_SALT = "<Insert server salt here>";
+    }
+}
